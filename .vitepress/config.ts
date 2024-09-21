685f7417af8500930ef72fd92dbe66cd3740f181
Hopetree <zlwork2014@outlook.com>
2024-09-21 12:30:09 +0800

    Sync from blog task

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -10,12 +10,16 @@ export default defineConfig({
     ['link', { rel: 'icon', href: '/img/favicon.png' }]
   ],
   themeConfig: {
+    // 这里设置显示的大纲层级
+    outline: {
+      level: [2, 4] // 显示 h2 到 h4 的标题
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
       { text: '个人博客', link: 'https://tendcode.com' }
     ],
-    // update date:2024-09-20 23:30:13
+    // update date:2024-09-21 12:30:08
     sidebar: {
   "/blog/1/": [
     {
