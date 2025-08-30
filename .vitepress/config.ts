74a66e2ae99f1f41e5980583c32252f49e8c7e58
Hopetree <zlwork2014@outlook.com>
2025-08-31 02:30:17 +0800

    Sync from blog task

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: '个人博客', link: 'https://tendcode.com' }
     ],
-    // update date:2025-08-27 02:30:17
+    // update date:2025-08-31 02:30:15
     sidebar: {
   "/blog/1/": [
     {
